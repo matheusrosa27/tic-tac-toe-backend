001da5de1e914e707b559d53f6505b68bc38b3a6
Matheus Fernandes da Rosa <matheusmemphis@gmail.com>
2024-04-22 21:40:53 -0300

    feat: adding cors feature

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from route import AppRoute
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Routes
 app.include_router(AppRoute.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
